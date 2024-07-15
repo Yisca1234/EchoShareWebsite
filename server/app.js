f9d978eb6cf1467f04d654b107252bfea10ab0b8
Yisca Grossman <gyisca@gmail.com>
2024-07-15 15:47:22 +0300

    fourth

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 require('express-async-errors');
-const cors = require('cors');
+// const cors = require('cors');
 const path = require('path');
 
 const authRoutes = require('./routes/auth');
@@ -17,7 +17,7 @@ const corsOptions = {
   maxAge: 600, // Cache the preflight response for 10 minutes
 };
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
